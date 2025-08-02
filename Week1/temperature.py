39754479962883579bb23d963513a1c9d1844c45
xinyuanhu1066 <270669474@yoobeestudent.ac.nz>
2025-08-03 09:57:43 +1200

    Activity 3: format prints

--- Week1/temperature.py
+++ Week1/temperature.py
@@ -6,23 +6,23 @@ days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sun
 def average(data):
     print('\nThe temperature for the week:')
     for index,temperature in enumerate(data):
-        print(f'   {days[index]}: {temperature} °C')
+        print(f'   {days[index]:10s} {temperature}°C')
     print()
     average = np.round(np.mean(data))
-    print(f'The average temperature is: {average} °C')
+    print(f'The average temperature is: {average}°C')
     print()
 
 
 def find_max_min(data):
-    print(f'\nThe highest temperature is {np.max(data)} °C')
-    print(f'The lowest temperature is {np.min(data)} °C\n')
+    print(f'\nThe highest temperature is {np.max(data)}°C')
+    print(f'The lowest  temperature is {np.min(data)}°C\n')
 
 
 def convert(data):
     print('\nThe temperatures in Fahrenheit:')
     for temperature in data:
         value = np.round(temperature * 9 / 5 + 32)
-        print(f'    {value} °F')
+        print(f'    {value}°F')
     print()
 
 
@@ -31,6 +31,7 @@ def filter_days(data):
     for index,temperature in enumerate(data):
         if temperature > 20:
             print(f'   {days[index]}')
+    print()
 
 
 if __name__ == '__main__':

8f3807a325cd63a1b46f007f88ade80659ae47b1
xinyuanhu1066 <270669474@yoobeestudent.ac.nz>
2025-08-10 08:42:22 +1200

    Update activity3.py

--- Week2/activity3.py
+++ Week2/activity3.py
@@ -1,6 +1,6 @@
 class StringManipulator:
-    def __init__(self, text):
-        self.text = text
+    # def __init__(self, text):
+    #     self.text = text
 
     def find_character(self, char):
         return self.text.find(char)
@@ -12,12 +12,19 @@ class StringManipulator:
         return self.text.upper()
 
 
-if __name__ == '__main__':
+def main():
     test_str = 'example'
     test_char = 'x'
-    name = StringManipulator(test_str)
+    # name = StringManipulator(test_str)
+    name = StringManipulator()
+    # assign the "text" attribute to the object dynamically
+    name.text = test_str
     index = name.find_character(test_char)
     print(f'\nThe text is: "{test_str}"')
     print(f'\nThe index of "{test_char}" is: {index}\n')
     print(f'The length of "{test_str}" is: {name.length()}')
     print(f'\nThe uppercase of "{test_str}" is: {name.upper()}\n')
+
+
+if __name__ == '__main__':
+    main()
